cf400eb5646d1c65162cb02a4df5b24b7dc2d9bb
philip139 <philip.nisem139@gmail.com>
2025-12-07 00:29:27 +0200

    add my changes

--- catalog/model/localisation/country.php
+++ catalog/model/localisation/country.php
@@ -6,17 +6,36 @@ class ModelLocalisationCountry extends Model {
 		return $query->row;
 	}
 
+	//philip here
+	// public function getCountries() {
+	// 	$country_data = $this->cache->get('country.catalog');
+
+	// 	if (!$country_data) {
+	// 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "country WHERE status = '1' ORDER BY name ASC");
+
+	// 		$country_data = $query->rows;
+
+	// 		$this->cache->set('country.catalog', $country_data);
+	// 	}
+
+	// 	return $country_data;
+	// }
 	public function getCountries() {
-		$country_data = $this->cache->get('country.catalog');
-
-		if (!$country_data) {
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "country WHERE status = '1' ORDER BY name ASC");
-
-			$country_data = $query->rows;
-
-			$this->cache->set('country.catalog', $country_data);
+		$query = $this->db->query("SELECT country_id, name, name_ar FROM " . DB_PREFIX . "country WHERE status = '1' ORDER BY name");
+	
+		$language = isset($this->session->data['language']) ? $this->session->data['language'] : 'en';
+		
+		$countries = [];
+		foreach ($query->rows as $row) {
+			$country_name = ($language == 'ar' && !empty($row['name_ar'])) ? $row['name_ar'] : $row['name'];
+			$countries[] = [
+				'country_id' => $row['country_id'],
+				'name'       => $country_name
+			];
 		}
-
-		return $country_data;
+	
+		return $countries;
 	}
+	//end philip here
+
 }
--- catalog/model/localisation/zone.php
+++ catalog/model/localisation/zone.php
@@ -5,18 +5,36 @@ class ModelLocalisationZone extends Model {
 
 		return $query->row;
 	}
+	// philip here
+	// public function getZonesByCountryId($country_id) {
+	// 	$zone_data = $this->cache->get('zone.' . (int)$country_id);
 
+	// 	if (!$zone_data) {
+	// 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone WHERE country_id = '" . (int)$country_id . "' AND status = '1' ORDER BY name");
+
+	// 		$zone_data = $query->rows;
+
+	// 		$this->cache->set('zone.' . (int)$country_id, $zone_data);
+	// 	}
+
+	// 	return $zone_data;
+	// }
 	public function getZonesByCountryId($country_id) {
-		$zone_data = $this->cache->get('zone.' . (int)$country_id);
-
-		if (!$zone_data) {
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone WHERE country_id = '" . (int)$country_id . "' AND status = '1' ORDER BY name");
-
-			$zone_data = $query->rows;
-
-			$this->cache->set('zone.' . (int)$country_id, $zone_data);
+		$query = $this->db->query("SELECT zone_id, name, name_ar FROM " . DB_PREFIX . "zone WHERE country_id = '" . (int)$country_id . "' AND status = '1' ORDER BY name");
+	
+		$language = isset($this->session->data['language']) ? $this->session->data['language'] : 'en';
+	
+		$zones = [];
+		foreach ($query->rows as $row) {
+			$zone_name = ($language == 'ar' && !empty($row['name_ar'])) ? $row['name_ar'] : $row['name'];
+			$zones[] = [
+				'zone_id' => $row['zone_id'],
+				'name'    => $zone_name
+			];
 		}
-
-		return $zone_data;
+	
+		return $zones;
 	}
+	//end philip here
+
 }
