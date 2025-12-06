f82d607d69629addfc68c009b3da201351156fb8
philip139 <philip.nisem139@gmail.com>
2025-12-07 00:10:39 +0200

    new version

---
+++ catalog/language/ar/extension/payment/paypal.php
@@ -0,0 +1,118 @@
+<?php
+// Text
+$_['text_paypal']							= 'باي بال';
+$_['text_paypal_fastlane']					= 'بطاقات الائتمان والخصم (فاستلين)';
+$_['text_paypal_title']						= 'باي بال (الدفع بحساب باي بال أو البطاقة الائتمانية)';
+$_['text_paypal_paylater_title']			= 'اشتري الآن وادفع لاحقاً بواسطة باي بال';
+$_['text_paypal_googlepay_title']			= 'جوجل باي - Google Pay';
+$_['text_paypal_applepay_title']			= 'آبل باي - Apple Pay';
+$_['text_paypal_fastlane_title']			= 'بطاقات الائتمان والخصم';
+$_['text_checkout_payment_address']  		= 'تفاصيل الفاتورة';
+$_['text_checkout_shipping_address'] 		= 'تفاصيل التسليم';
+$_['text_checkout_shipping_method']  		= 'طريقة التوصيل';
+$_['text_checkout_payment_method']  		= 'طريقة الدفع او السداد';
+$_['text_your_details']              		= 'بياناتك الشخصية';
+$_['text_your_address']              		= 'عنوانك';
+$_['text_cart']               				= 'عربة التسوق';
+$_['text_shipping_updated']   				= 'تم تحديث خدمة الشحن';
+$_['text_fastlane']                         = 'فاستلين';
+$_['text_fastlane_shipping']                = 'الشحن';
+$_['text_fastlane_payment']                 = 'الدفع';
+$_['text_day']                 				= 'يوم';
+$_['text_week']                				= 'اسبوع';
+$_['text_semi_month']          				= 'منتصف الشهر';
+$_['text_month']               				= 'شهر';
+$_['text_year']                				= 'سنة';
+$_['text_card_number']          			= '%s ينتهي في %s';
+$_['text_trial']               				= '%s كل %s %s الى %s المدفوعات بعد ذلك ';
+$_['text_recurring']          				= '%s كل %s %s';
+$_['text_recurring_item']      				= 'التكرار';
+$_['text_payment_recurring']   				= 'الملف الشخصي للدفع';
+$_['text_trial_description']   				= '%s كل %d %s(s) الى %d مدفوعات بعد ذلك';
+$_['text_payment_description'] 				= '%s كل %d %s(s) الى %d مدفوعات';
+$_['text_payment_cancel']      				= '%s كل %d %s(s) حتى الإلغاء';
+$_['text_length']							= ' لـ %s مدفوعات';
+$_['text_order_message']					= 'حماية البائع عبر PayPal - %s';
+$_['text_wait']								= 'انتظر من فضلك !';
+$_['text_loading']          				= 'جاري ...';
+$_['text_failure_page_title']				= 'لقد فشل طلبك!';
+$_['text_failure_page_message']				= 'عذراً، لكن المعاملة فشلت! يرجى اختيار طريقة دفع أخرى أو <a href="%s" target="_blank">اتصل بنا</a>';
+
+// Column
+$_['column_image']             				= 'صورة';
+$_['column_name']              				= 'المنتج';
+$_['column_model']             				= 'الموديل';
+$_['column_quantity']          				= 'الكمية';
+$_['column_price']             				= 'سعر الوحدة';
+$_['column_total']             				= 'المجموع';
+
+// Entry
+$_['entry_email']                    		= 'البريد الالكتروني';
+$_['entry_firstname']                		= 'الاسم الأول';
+$_['entry_lastname']                 		= 'اسم العائلة';
+$_['entry_telephone']               		= 'هاتف';
+$_['entry_company']                  		= 'شركة';
+$_['entry_address_1']                		= 'العنوان 1';
+$_['entry_address_2']                		= 'العنوان 2';
+$_['entry_postcode']                 		= 'الرمز البريدي';
+$_['entry_city']                     		= 'مدينة';
+$_['entry_country']                  		= 'دولة';
+$_['entry_zone']                    		= 'المنطقة / المحافظة';
+$_['entry_card_number']						= 'رقم البطاقة';
+$_['entry_expiration_date']					= 'تاريخ الإنتهاء';
+$_['entry_cvv']								= 'رمز CVV';
+$_['entry_card_save']						= 'حفظ بطاقتك';
+$_['entry_shipping'] 				    	= 'هذه العملية تتطلب شحن';
+
+// Button
+$_['button_confirm']  						= 'تأكيد';
+$_['button_continue']  						= 'متابعة';
+$_['button_shipping'] 						= 'تحديث الشحن';
+$_['button_pay']							= 'الدفع بالبطاقة';
+$_['button_edit']  							= 'تعديل';
+
+// Success
+$_['success_order'] 			    		= 'نجاح: لقد قمت بتعديل الطلبات!';
+
+// Error
+$_['error_warning']							= 'يرجى التحقق من النموذج بعناية للتأكد من عدم وجود أخطاء.';
+$_['error_stock']              				= 'المنتجات التي تحمل علامة *** غير متوفرة بالكمية المطلوبة أو غير متوفرة في المخزون !';
+$_['error_minimum']            				= 'الحد الأدنى لمبلغ الطلب لـ %s هو %s !';
+$_['error_required']           				= '%s مطلوب !';
+$_['error_product']            				= 'تنبيه: لا توجد منتجات في سلة التسوق الخاصة بك !';
+$_['error_recurring_required'] 				= 'Please select a payment recurring!';
+$_['error_unavailable'] 	  				= 'يرجى استخدام الدفع الكامل مع هذا الطلب !';
+$_['error_shipping']                 		= 'تنبيه: طريقة الشحن مطلوبة !';
+$_['error_no_shipping']    					= 'تنبيه: لا تتوفر خيارات الشحن.';
+$_['error_firstname']                		= 'يجب أن يتراوح الاسم الأول بين 1 و32 حرفًا !';
+$_['error_lastname']                 		= 'يجب أن يتراوح اسم العائلة بين 1 و32 حرفًا !';
+$_['error_email']                    		= 'يبدو أن عنوان البريد الإلكتروني غير صالح !';
+$_['error_telephone']                		= 'يجب أن يتراوح عدد أرقام الهاتف بين 3 و32 حرفًا !';
+$_['error_password']                 		= 'يجب أن تتراوح كلمة المرور بين 4 و20 حرفًا !';
+$_['error_confirm']                  		= 'تأكيد كلمة المرور لا يتطابق مع كلمة المرور !';
+$_['error_address_1']                		= 'يجب أن يتراوح العنوان 1 بين 3 و128 حرفًا !';
+$_['error_city']                     		= 'يجب أن يتراوح اسم المدينة بين 2 و128 حرفًا !';
+$_['error_postcode']                 		= 'يجب أن يتراوح الرمز البريدي بين 2 و10 أحرف !';
+$_['error_country']                  		= 'الرجاء اختيار البلد !';
+$_['error_zone']                     		= 'الرجاء تحديد المنطقة / المحافظة !';
+$_['error_agree']                    		= 'تنبيه: يجب عليك الموافقة على %s!';
+$_['error_address']                  		= 'تنبيه: يجب عليك تحديد العنوان !';
+$_['error_custom_field']             		= '%s مطلوب !';
+$_['error_fastlane_billing_address'] 		= 'عنوان الفاتورة لا يبدو أنه صالح!';
+$_['error_order_voided']					= 'لم نتمكن من معالجة دفعتك. سيتم إلغاء جميع المنتجات في الطلب. الرجاء <a href="%s" target="_blank">مراسلتنا</a>.';
+$_['error_order_completed']					= 'We could not process your payment. The payment was authorized or the authorized payment was captured for the order. الرجاء <a href="%s" target="_blank">مراسلتنا</a>.';
+$_['error_authorization_captured']			= 'We could not process your payment. The authorized payment has one or more captures against it. The sum of these captured payments is greater than the amount of the original authorized payment. الرجاء <a href="%s" target="_blank">مراسلتنا</a>.';
+$_['error_authorization_denied']			= 'We could not process the transaction with this card. The funds could not be captured. الرجاء try a different funding source.';
+$_['error_authorization_expired']			= 'We could not process your payment. The authorized payment has expired. الرجاء <a href="%s" target="_blank">مراسلتنا</a>.';
+$_['error_capture_declined']				= 'We could not process the transaction with this card. The funds could not be captured. الرجاء try a different funding source.';
+$_['error_capture_failed']					= 'We could not process your payment. There was an error while capturing payment. الرجاء <a href="%s" target="_blank">مراسلتنا</a>.';
+$_['error_3ds_failed_authentication']		= 'We could not process the transaction with this card. You may have failed the challenge or the device was not verified.';
+$_['error_3ds_rejected_authentication']		= 'We could not process the transaction with this card. 3D Secure authentication was skipped by you.';
+$_['error_3ds_attempted_authentication'] 	= 'We could not process the transaction with this card. Card is not enrolled in 3D Secure as card issuing bank is not participating in 3D Secure.';
+$_['error_3ds_unable_authentication']		= 'We could not process the transaction with this card. Issuing bank is not able to complete authentication.';
+$_['error_3ds_challenge_authentication']	= 'We could not process the transaction with this card. Challenge required for authentication.';
+$_['error_3ds_card_ineligible']				= 'We could not process the transaction with this card. Card type and issuing bank are not ready to complete a 3D Secure authentication.';
+$_['error_3ds_system_unavailable']			= 'We could not process the transaction with this card. An error occurred with the 3D Secure authentication system.';
+$_['error_3ds_system_bypassed'] 			= 'We could not process the transaction with this card. 3D Secure was skipped as authentication system did not require a challenge.';
+$_['error_payment']							= 'Please choose another payment method or <a href="%s" target="_blank">مراسلتنا</a>.';
+$_['error_timeout'] 	  					= 'Sorry, PayPal is currently busy. Please try again later!';
